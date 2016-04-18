15a56b9f51317c04edf60ae94e7a62e5fe2ba8eb
José Carlos <jose__carlos17@hotmail.com>
2016-04-18 13:31:11 +0100

    Menu manager implemented

---
+++ EasyPilot/src/source.cpp
@@ -0,0 +1,48 @@
+#include "EasyPilot.h"
+
+void menuManager()
+{
+	vector<string> menuPrincipal;
+	menuPrincipal.push_back("Menu principal");
+	menuPrincipal.push_back("Escolher mapa");
+	menuPrincipal.push_back("Sair");
+
+	vector<string> escolherMapa;
+	escolherMapa.push_back("Escolher mapa");
+	escolherMapa.push_back("Porto");
+	escolherMapa.push_back("Maia");
+	escolherMapa.push_back("Algarve");
+	escolherMapa.push_back("Sair");
+
+	int select = menuOptions(menuPrincipal);
+
+	while (true) {
+		switch (select) {
+		case 1:
+			select = menuOptions(escolherMapa);
+
+			switch (select) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				break;
+			}
+
+			break;
+
+		case 2:
+			cout << "Bye!\n";
+			exit(0);
+		}
+
+		select = menuOptions(menuPrincipal);
+	}
+}
+
+int main() {
+
+	menuManager();
+
+	return 0;
+}
